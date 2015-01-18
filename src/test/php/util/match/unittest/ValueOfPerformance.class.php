00fa7c62ea1e605d3114473f524e6a292b42c740
Timm Friebe <thekid@thekid.de>
2015-01-18 23:12:49 +0100

    Also include switch() statement

--- src/test/php/util/match/unittest/ValueOfPerformance.class.php
+++ src/test/php/util/match/unittest/ValueOfPerformance.class.php
@@ -3,7 +3,7 @@
 use util\match\ValueOf;
 
 class ValueOfPerformance extends \util\profiling\Measurable {
-  private $match, $native;
+  private $match, $if, $switch;
 
   public function __construct($method, $args) {
     parent::__construct($method, $args);
@@ -13,7 +13,7 @@ class ValueOfPerformance extends \util\profiling\Measurable {
       ->otherwise(function($value) { return $value.' elements'; })
     ;
 
-    $this->native= function($value) {
+    $this->if= function($value) {
       if (0 === $value) {
         return 'No elements';
       } else if (1 === $value) {
@@ -22,10 +22,18 @@ class ValueOfPerformance extends \util\profiling\Measurable {
         return $value.' elements';
       }
     };
+
+    $this->switch= function($value) {
+      switch ($value) {
+        case 0: return 'No elements';
+        case 1: return 'One element';
+        default: return  $value.' elements';
+      }
+    };
   }
 
   /** @return var[][] */
-  public static function values() { return [0, 1, 2]; }
+  public static function values() { return [0, 1, 2, 100]; }
 
   #[@measure, @values('values')]
   public function match($value) {
@@ -34,8 +42,14 @@ class ValueOfPerformance extends \util\profiling\Measurable {
   }
 
   #[@measure, @values('values')]
-  public function native($value) {
-    $f= $this->native;
+  public function if_stmt($value) {
+    $f= $this->if;
+    return $f($value);
+  }
+
+  #[@measure, @values('values')]
+  public function swicht_stmt($value) {
+    $f= $this->switch;
     return $f($value);
   }
 }
